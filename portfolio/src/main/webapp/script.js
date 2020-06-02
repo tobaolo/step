ab68dbad0fae1c50e1a68908e9ec60c821a63335
Toba Olokungbemi <tobaolokungbemi@gmail.com>
2020-06-02 16:20:09 +0000

    Fixed indentation as per style guide

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,9 +16,9 @@
  * Fetches a welcome messgae to the home page
  */
 function sayWelcome() {
-    fetch('/data').then(response => response.text()).then((text) => {
-        document.getElementById('home-welcome').innerText = text;
-    })
+  fetch('/data').then(response => response.text()).then((text) => {
+    document.getElementById('home-welcome').innerText = text;
+  })
 }
 /**
  * Adds a random quote to the page.
