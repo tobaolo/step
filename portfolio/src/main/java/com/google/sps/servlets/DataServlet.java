1e85328140e94b4fa2df3fc276c7fbcc28f9d5f5
Toba Olokungbemi <tobaolokungbemi@gmail.com>
2020-06-08 14:22:51 +0000

    Change default select value to '' and modfiy appropriate code in java to handle passing ''

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -83,8 +83,13 @@ public class DataServlet extends HttpServlet {
     String commentLimitString = request.getParameter("limit-comments");
 
     // Convert the input to an int.
-    int commentLimit = Integer.parseInt(commentLimitString);
-
+    int commentLimit;
+    if (commentLimitString.equals("")) {
+      commentLimit = 1000;
+    } else {
+      commentLimit = Integer.parseInt(commentLimitString);
+    }
+    
     return commentLimit;
   }
 }
