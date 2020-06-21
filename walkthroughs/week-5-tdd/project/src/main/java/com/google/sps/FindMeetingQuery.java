707b9ab24041a015c3d457a40b93bab9309df59e
Toba Olokungbemi <tobaolokungbemi@gmail.com>
2020-06-21 01:29:35 +0000

    Run Java Formatter

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,18 +14,16 @@
 
 package com.google.sps;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-
+    
     // Checks if duration of request is longer than duration of day.
     if (request.getDuration() > (TimeRange.END_OF_DAY - TimeRange.START_OF_DAY + 1)) {
       Collection<TimeRange> empty = new ArrayList<TimeRange>();
@@ -39,21 +37,20 @@ public final class FindMeetingQuery {
     Set<String> attendeesList = new HashSet<>(request.getAttendees());
 
     // Returns the whole day if there are no attendees.
-    if (attendeesList.size() == 0){
+    if (attendeesList.size() == 0) {
       Collection<TimeRange> allDayList = new ArrayList<TimeRange>();
       allDayList.add(allDay);
       return allDayList;
     }
 
     // Creates a map wth the key as the perosn's name and an empty list of their events.
-    HashMap<String, ArrayList<Event>> scheduleMap = 
-        new HashMap<String, ArrayList<Event>>();
+    HashMap<String, ArrayList<Event>> scheduleMap = new HashMap<String, ArrayList<Event>>();
 
     // Add a key (name of atendee) and empty list of attendee's events.
     for (String name : attendeesList) {
       scheduleMap.put(name, new ArrayList<Event>());
     }
-    
+
     // Iterate through all events, check if one of the requested attendees is in the listed event,
     // and add to each person's individual schedule.
     for (Event event : events) {
@@ -82,6 +79,7 @@ public final class FindMeetingQuery {
       if (scheduleList.isEmpty()) {
         freeTimeList.add(allDay);
       } else {
+
         // Iterate through each event in the schedule and calculate free time between.
         // Start index 0 at beginning of day.
         for (int i = 0; i <= scheduleSize; i++) {
@@ -99,13 +97,13 @@ public final class FindMeetingQuery {
 
           // From the end of the last event to the end of the day.
           } else if (refInt.equals(scheduleSize)) {
-            previousEventTimeRange = scheduleList.get(i-1).getWhen();
+            previousEventTimeRange = scheduleList.get(i - 1).getWhen();
             startFreeTime = previousEventTimeRange.end();
             endFreeTime = TimeRange.END_OF_DAY + 1;
 
           // In between events during the day.
           } else {
-            previousEventTimeRange = scheduleList.get(i-1).getWhen();
+            previousEventTimeRange = scheduleList.get(i - 1).getWhen();
             startFreeTime = previousEventTimeRange.end();
             nextEventTimeRange = scheduleList.get(i).getWhen();
             endFreeTime = nextEventTimeRange.start();
@@ -119,7 +117,6 @@ public final class FindMeetingQuery {
           }
         }
       }
-
       availabilityMap.put(name, freeTimeList);
     }
 
@@ -128,8 +125,8 @@ public final class FindMeetingQuery {
       return null;
     }
 
-    ArrayList<ArrayList<TimeRange>> freeTimeList = new ArrayList<ArrayList<TimeRange>>
-        (availabilityMap.values());
+    ArrayList<ArrayList<TimeRange>> freeTimeList =
+        new ArrayList<ArrayList<TimeRange>>(availabilityMap.values());
     ArrayList<TimeRange> finalList = getAvailable(freeTimeList, request.getDuration());
     System.out.println(finalList);
 
@@ -137,64 +134,61 @@ public final class FindMeetingQuery {
   }
 
   /**
-   * Compares time ranges and returns a time range of the overlap. Boolean nextPerson checks 
-   * whether there is another person to compare time ranges with.
+   * Compares time ranges and returns a time range of the overlap. Boolean nextPerson checks whether
+   * there is another person to compare time ranges with.
    */
-  private ArrayList<TimeRange> getAvailable(ArrayList<ArrayList<TimeRange>> allFreeTimeList, 
-      long requestDuration) {
-        ArrayList<TimeRange> availableOverlapList = new ArrayList<TimeRange>();
-        Integer allFreeTimeListSize = new Integer(allFreeTimeList.size());
-        
-        // Validates the list is not empty.
-        if (allFreeTimeListSize.equals(0)) {
-          return null;
-        }
+  private ArrayList<TimeRange> getAvailable(
+      ArrayList<ArrayList<TimeRange>> allFreeTimeList, long requestDuration) {
+    ArrayList<TimeRange> availableOverlapList = new ArrayList<TimeRange>();
+    Integer allFreeTimeListSize = new Integer(allFreeTimeList.size());
 
-        // If only one person is requestd, return their free time.
-        if (allFreeTimeListSize.equals(1)) {
-          return allFreeTimeList.get(0);
-        }
+    // Validates the list is not empty.
+    if (allFreeTimeListSize.equals(0)) {
+      return null;
+    }
 
-        // Compares the first 2 TimeRange in freeTimeList and returns the overlap if only 2 
-        // elements in freeTimeList or calls getAvailable recursively by replacing the first 2
-        // TimeRanges with their overlapped TimeRange
-        ArrayList<TimeRange> freeTimeList1 = allFreeTimeList.get(0);
-        ArrayList<TimeRange> freeTimeList2 = allFreeTimeList.get(1);
+    // If only one person is requestd, return their free time.
+    if (allFreeTimeListSize.equals(1)) {
+      return allFreeTimeList.get(0);
+    }
 
-        // Compares each time range for the second person to the time ranges of the first person.
-        for (TimeRange range1 : freeTimeList1) {
-          for (TimeRange range2 : freeTimeList2) {
+    // Compares the first 2 TimeRange in freeTimeList and returns the overlap if only 2
+    // elements in freeTimeList or calls getAvailable recursively by replacing the first 2
+    // TimeRanges with their overlapped TimeRange
+    ArrayList<TimeRange> freeTimeList1 = allFreeTimeList.get(0);
+    ArrayList<TimeRange> freeTimeList2 = allFreeTimeList.get(1);
 
-            // Checks if there is an overlap between the 2 ranges.
-            if (range1.overlaps(range2)) {
-              long overlapStart = Math.max(range1.start(), range2.start());
-              long overlapEnd = Math.min(range1.end(), range2.end());
-              long overlapDuration = overlapEnd - overlapStart;
-            
-              // Verifies that the overlap duration is greater than or equal to request duration.
-              if (overlapDuration >= requestDuration) {
-                TimeRange overlapTimeRange = TimeRange
-                    .fromStartDuration((int) overlapStart, (int) overlapDuration);
+    // Compares each time range for the second person to the time ranges of the first person.
+    for (TimeRange range1 : freeTimeList1) {
+      for (TimeRange range2 : freeTimeList2) {
 
-                // If there are more requested attendees in the allFreeTimeList, must compare their
-                // free time with the overlapped free time of previous requested attendees.
-                if (allFreeTimeListSize > 2) {
-                  ArrayList<TimeRange> overlapArray = new ArrayList<TimeRange>();
-                  overlapArray.add(overlapTimeRange);
-                  ArrayList<ArrayList<TimeRange>> newAllFreeTimeList = new ArrayList
-                      <ArrayList<TimeRange>>(allFreeTimeList.subList(2, allFreeTimeListSize));
-                  newAllFreeTimeList.add(0, overlapArray);
-                  availableOverlapList.addAll(getAvailable(newAllFreeTimeList, requestDuration));
-                }
+        // Checks if there is an overlap between the 2 ranges.
+        if (range1.overlaps(range2)) {
+          long overlapStart = Math.max(range1.start(), range2.start());
+          long overlapEnd = Math.min(range1.end(), range2.end());
+          long overlapDuration = overlapEnd - overlapStart;
 
-                availableOverlapList.add(overlapTimeRange);
-              }
+          // Verifies that the overlap duration is greater than or equal to request duration.
+          if (overlapDuration >= requestDuration) {
+            TimeRange overlapTimeRange =
+                TimeRange.fromStartDuration((int) overlapStart, (int) overlapDuration);
+                
+            // If there are more requested attendees in the allFreeTimeList, must compare their
+            // free time with the overlapped free time of previous requested attendees.
+            if (allFreeTimeListSize > 2) {
+              ArrayList<TimeRange> overlapArray = new ArrayList<TimeRange>();
+              overlapArray.add(overlapTimeRange);
+              ArrayList<ArrayList<TimeRange>> newAllFreeTimeList =
+                  new ArrayList<ArrayList<TimeRange>>(
+                      allFreeTimeList.subList(2, allFreeTimeListSize));
+              newAllFreeTimeList.add(0, overlapArray);
+              availableOverlapList.addAll(getAvailable(newAllFreeTimeList, requestDuration));
             }
+            availableOverlapList.add(overlapTimeRange);
           }
         }
-        
-        return availableOverlapList;
+      }
+    }
+    return availableOverlapList;
   }
 }
-
-
